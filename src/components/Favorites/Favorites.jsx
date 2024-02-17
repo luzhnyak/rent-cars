ca7e6ca0636034bc0131695e8818801d5a0672f7
Oleh Luzhniak <oleg.luzhnyak@gmail.com>
2024-02-18 01:37:17 +0200

    add no select cars

--- src/components/Favorites/Favorites.jsx
+++ src/components/Favorites/Favorites.jsx
@@ -5,6 +5,7 @@ import css from "./Favorites.module.css";
 import { selectAllFavoritesCars } from "../../redux/favorites/selectors";
 
 import Card from "../Card/Card";
+import { Link } from "react-router-dom";
 
 const Favorites = () => {
   const cars = useSelector(selectAllFavoritesCars);
@@ -13,10 +14,16 @@ const Favorites = () => {
     <>
       <div className={css.container}>
         <section className={css.sectionCatalog}>
-          {cars.length &&
+          {cars?.length > 0 ? (
             cars.map((car) => {
               return <Card key={car.id} data={car} />;
-            })}
+            })
+          ) : (
+            <p className={css.noSelect}>
+              No selected cars yet. Explore our{" "}
+              <Link to="/catalog">catalog</Link> now.
+            </p>
+          )}
         </section>
       </div>
     </>
